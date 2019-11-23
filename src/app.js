62c07b0a261a8f40726d2d0f4a6957863d4cced7
Denisa <tdenisamihaela@yahoo.com>
2019-11-23 17:08:55 +0000

    Movie-project

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Main from './Components/main'
+import Navbar from './Components/Navbar'
+import SingleMovie from './Components/singleMovie'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import 'bulma'
 import './styles/style.scss'
 
-class App extends React.Component {
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/main/:id" component={SingleMovie} />
+      <Route exact path="/" component={Main} />
+      {/* <Route exact path="/EditMovie" component={EditMovie} /> */}
+      {/* <Route path="/watchlater" component={Favourites} /> */}
+      
+    </Switch>
+  </BrowserRouter>
+)
 
-  constructor() {
-    super()
-    this.state = {}
-  }
 
-  render() {
-    return (      
-      <h1>Hello world</h1>
-    
-    )
-    
-  }
-}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
-  console.log('45d10d2c7c32e0e63ea28fb1c6b321ef')
 )
